d7668dc42a606c6d13d5477f27be4f2753b194e3
inhyeok <ss112d86@gmail.com>
2024-06-17 15:16:44 +0900

    slack update

--- .github/actions/customslack/dist/index.js
+++ .github/actions/customslack/dist/index.js
@@ -30789,8 +30789,10 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(8811);
 const fetch = __nccwpck_require__(183);
+const fs = __nccwpck_require__(7147);
+const CHAT_POST_MESSAGE_URL = "https://slack.com/api/chat.postMessage";
 
-function generatePayload({ text, channel, color, prTitle, author, repo, url }) {
+function generatePRPayload({ text, channel, color, prTitle, author, repo, url }) {
   return {
     text,
     channel,
@@ -30848,31 +30850,85 @@ function generatePayload({ text, channel, color, prTitle, author, repo, url }) {
   };
 }
 
-async function run() {
-  const CHAT_POST_MESSAGE_URL = "https://slack.com/api/chat.postMessage";
-  const { GITHUB_REPOSITORY, T_TEST } = process.env;
-  console.log(T_TEST);
-  try {
-    const prTitle = core.getInput("prTitle");
-    const author = core.getInput("author");
-    const statusColor = core.getInput("statusColor");
-    const channelId = core.getInput("channelId");
-    const repo = GITHUB_REPOSITORY;
-    const url = core.getInput("url");
-    const slackToken = core.getInput("slackToken");
+function validateToken(token) {
+  if (token === undefined || token == null || token.length == 0) {
+    throw new Error("invalidate token");
+  }
+}
+
+async function sendSlackMessage(slackToken, payload) {
+  const response = await fetch(CHAT_POST_MESSAGE_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json", Authorization: `Bearer ${slackToken}` },
+    body: JSON.stringify(payload),
+  });
+
+  const responseData = await response.json();
+  return responseData;
+}
+
+function selectPayload(mode) {
+  if (mode == "PR") {
+    const { GITHUB_REPOSITORY, GITHUB_EVENT_PATH } = process.env;
+    if (!GITHUB_EVENT_PATH) {
+      throw new Error("GITHUB_EVENT_PATH is not defined");
+    }
+    const eventData = JSON.parse(fs.readFileSync(GITHUB_EVENT_PATH, "utf8"));
 
     const text = "GITHUB CI 결과";
+    const channel = core.getInput("channelId");
+    const color = core.getInput("statusColor");
 
-    const payload = generatePayload({ text, channel: channelId, color: statusColor, prTitle, author, repo, url });
+    if (eventData.pull_request == undefined) {
+      throw new Error("PR data is undefined");
+    }
 
-    const response = await fetch(CHAT_POST_MESSAGE_URL, {
-      method: "POST",
-      headers: { "Content-Type": "application/json", Authorization: `Bearer ${slackToken}` },
-      body: JSON.stringify(payload),
+    const pullRequest = eventData.pull_request;
+    const prTitle = pullRequest.title;
+    const author = pullRequest.user.login;
+    const url = pullRequest.html_url;
+
+    const repo = GITHUB_REPOSITORY;
+
+    const genpayload = generatePRPayload({
+      text,
+      channel,
+      color,
+      prTitle,
+      author,
+      url,
+      repo,
     });
+    return genpayload;
+  } else {
+    throw new Error("[MODE SELECT]not support Mode");
+  }
+}
 
-    const responseData = await response.json();
-    console.log(responseData);
+async function run() {
+  const { SLACK_TOKEN } = process.env;
+
+  try {
+    validateToken(SLACK_TOKEN);
+
+    // payload가 있으면 payload를 그대로 리턴한다.
+    const payload = core.getMultilineInput("payload");
+    if (payload != undefined && payload.length != 0) {
+      console.error("[payload send]not support function");
+      console.error(payload);
+      throw new Error("[payload send]not support function");
+    }
+
+    // payload가 없으면 mode 단위로 처리한다.
+    const mode = core.getInput("mode");
+    const selectedPayload = selectPayload(mode);
+
+    const response = await sendSlackMessage(SLACK_TOKEN, selectedPayload);
+    if (response.ok == false) {
+      throw new Error("send to message fail..!");
+    }
+
+    core.setOutput("ts", response.ts); // ts 셋팅, 나중에 이걸로 인터렉션 가능
   } catch (error) {
     core.setFailed(`Action failed with error ${error}`);
   }
